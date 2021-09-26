0fb20a38381522d83bbfe58aad2cb269770b0ae9
kayedavi <kayedavi@gmail.com>
2021-09-25 23:17:26 -0600

    Changed the implementation of Option.traverse

--- src/chapter04/option.py
+++ src/chapter04/option.py
@@ -131,7 +131,11 @@ def sequence_1(a: List[Option[A]]) -> Option[List[A]]:
 
 
 def traverse(a: List[A], f: Callable[[A], Option[B]]) -> Option[List[B]]:
-    return a.fold_right(some(empty_list()), (lambda h, t: map2(f(h), t, Cons)))
+    match a:
+        case Nil():
+            return Some(Nil())
+        case Cons(h, t):
+            return map2(f(h), traverse(t, f), Cons)
 
 
 def traverse_1(a: List[A], f: Callable[[A], Option[B]]) -> Option[List[B]]:
